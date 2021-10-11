fc8fb22be9e21e7344f2c1d7c405cc1dfa4e770a
Mateus Pereira <mateus.p.jorge@gmail.com>
2021-10-11 09:57:21 -0300

    Finalizando os testes dos estados

--- LocationServer/src/test/java/br/com/locationServer/services/StateServiceTest.java
+++ LocationServer/src/test/java/br/com/locationServer/services/StateServiceTest.java
@@ -3,6 +3,9 @@ package br.com.locationServer.services;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
@@ -40,7 +43,8 @@ public class StateServiceTest {
 	@Test
 	public void shouldThrowErrorOfRegisterStateIfCountryIdFieldAreMissing() throws StateException {
 		when(stateRepository.save(any())).thenReturn(createState(1L, 1L, "Paraíba", "PB"));
-		Assert.assertThrows(StateException.class, () -> stateService.registerState(createStateDto(null, "Paraíba", "PB")));
+		Assert.assertThrows(StateException.class,
+				() -> stateService.registerState(createStateDto(null, "Paraíba", "PB")));
 	}
 
 	@Test
@@ -52,24 +56,102 @@ public class StateServiceTest {
 	@Test
 	public void shouldThrowErrorOfRegisterStateIfInitialsFieldAreMissing() throws StateException {
 		when(stateRepository.save(any())).thenReturn(createState(1L, 1L, "Paraíba", "PB"));
-		Assert.assertThrows(StateException.class, () -> stateService.registerState(createStateDto(1L, "Paraíba", null)));
+		Assert.assertThrows(StateException.class,
+				() -> stateService.registerState(createStateDto(1L, "Paraíba", null)));
+	}
+
+	@Test
+	public void shouldListAllStatesSuccessfully() throws StateException {
+		when(stateRepository.findAll()).thenReturn(Arrays.asList(createState(1L, 1L, "Paraíba", "PB")));
+		Assert.assertEquals(1, stateService.searchAllStates().size());
+	}
+
+	@Test
+	public void shouldThrowErrorOfStateListEmpty() throws StateException {
+		when(stateRepository.findAll()).thenReturn(Arrays.asList());
+		Assert.assertThrows(StateException.class, () -> stateService.searchAllStates());
+	}
+
+	@Test
+	public void shouldUpdatedStateSuccessfully() throws StateException {
+		when(stateRepository.findByCountryIdAndNameAndInitials(any(), any(), any()))
+				.thenReturn(createState(1L, 1L, "Paraíba", "PB"));
+		when(stateRepository.save(any())).thenReturn(createState(1L, 1L, "Paraíba", "PB"));
+		Assert.assertNotNull(stateService.updateState(createStateDto(1L, "Paraíba", "PB")));
+	}
+
+	@Test
+	public void shouldThrowErrorOfUpdatedStateIfCountryIdFieldAreMissing() throws StateException {
+		when(stateRepository.save(any())).thenReturn(createState(1L, 1L, "Paraíba", "PB"));
+		Assert.assertThrows(StateException.class,
+				() -> stateService.updateState(createStateDto(null, "Paraíba", "PB")));
+	}
+
+	@Test
+	public void shouldThrowErrorOfUpdatedStateIfNameFieldAreMissing() throws StateException {
+		when(stateRepository.save(any())).thenReturn(createState(1L, 1L, "Paraíba", "PB"));
+		Assert.assertThrows(StateException.class, () -> stateService.updateState(createStateDto(1L, null, "PB")));
+	}
+
+	@Test
+	public void shouldThrowErrorOfUpdatedStateIfInitialsFieldAreMissing() throws StateException {
+		when(stateRepository.save(any())).thenReturn(createState(1L, 1L, "Paraíba", "PB"));
+		Assert.assertThrows(StateException.class, () -> stateService.updateState(createStateDto(1L, "Paraíba", null)));
+	}
+
+	@Test
+	public void shouldThrowErrorOfUpdatedStateIfStateNotFound() throws StateException {
+		when(stateRepository.findByCountryIdAndNameAndInitials(any(), any(), any())).thenReturn(null);
+		Assert.assertThrows(StateException.class, () -> stateService.updateState(createStateDto(1L, "Paraíba", "PB")));
+	}
+
+	@Test
+	public void shouldDeleteStateSuccessfully() throws StateException {
+		when(stateRepository.findByCountryIdAndNameAndInitials(any(), any(), any()))
+				.thenReturn(createState(1L, 1L, "Paraíba", "PB"));
+		stateService.deleteState(createStateDto(1L, "Paraíba", "PB"));
+	}
+
+	@Test
+	public void shouldThrowErrorOfDeleteStateIfCountryIdFieldAreMissing() throws StateException {
+		Assert.assertThrows(StateException.class,
+				() -> stateService.deleteState(createStateDto(null, "Paraíba", "PB")));
+	}
+
+	@Test
+	public void shouldThrowErrorOfDeleteStateIfNameFieldAreMissing() throws StateException {
+		Assert.assertThrows(StateException.class, () -> stateService.deleteState(createStateDto(1L, null, "PB")));
+	}
+
+	@Test
+	public void shouldThrowErrorOfDeleteStateIfInitialsFieldAreMissing() throws StateException {
+		Assert.assertThrows(StateException.class, () -> stateService.deleteState(createStateDto(1L, "Paraíba", null)));
+	}
+
+	@Test
+	public void shouldThrowErrorOfDeleteStateIfStateNotFound() throws StateException {
+		when(stateRepository.findByCountryIdAndNameAndInitials(any(), any(), any())).thenReturn(null);
+		Assert.assertThrows(StateException.class, () -> stateService.updateState(createStateDto(1L, "Paraíba", "PB")));
+	}
+
+	@Test
+	public void shouldFoundStateSuccessfully() throws StateException {
+		when(stateRepository.findById(any())).thenReturn(Optional.of(createState(1L, 1L, "Paraíba", "PB")));
+		Assert.assertNotNull(stateService.searchStateById(1L));
+	}
+
+	@Test
+	public void shouldThrowErrorOfSearchStateByIdIfIdFieldNotFound() throws StateException {
+		when(stateRepository.findById(any())).thenReturn(Optional.empty());
+		Assert.assertThrows(StateException.class, () -> stateService.searchStateById(1L));
 	}
 
 	private State createState(Long id, Long countryId, String name, String inicials) {
-		return State.builder()
-				    .id(id)
-				    .countryId(countryId)
-				    .name(name)
-				    .initials(inicials)
-				    .build();
+		return State.builder().id(id).countryId(countryId).name(name).initials(inicials).build();
 	}
 
 	private StateDTO createStateDto(Long countryId, String name, String inicials) {
-		return StateDTO.builder()
-					   .countryId(countryId)
-					   .name(name)
-					   .initials(inicials)
-					   .build();
+		return StateDTO.builder().countryId(countryId).name(name).initials(inicials).build();
 	}
 
 }
